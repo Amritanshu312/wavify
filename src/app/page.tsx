cd27122423be601173b36ee786abe4b1d52b5523
Amritanshu312 <ffdarkrayon@gmail.com>
2024-12-09 21:38:03 +0530

    Better tree structure: UserInfo && WhyJoinUs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import UserInfoHome from "@/components/layout/User/UserInfoHome";
-import WhyJoinWavify from "@/components/layout/User/WhyJoinWavify";
+import UserInfoHome from "@/content/Home/User/UserInfoHome";
+import WhyJoinWavify from "@/content/Home/User/WhyJoinWavify";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
+
 export default function Home() {
   return (
     <div className="w-full flex justify-center my-4">
